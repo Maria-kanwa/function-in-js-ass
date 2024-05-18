257bb73f81494823f34534f7e429e7209c9e7d99
Maria kanwal <you@example.com>
2024-05-18 20:15:25 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function table(num1,num2){
+   
+    for(let num2=0;num2<=20;num2++){
+        console.log((num1 + " * " + num2+ " = " + num1* num2 ));   
+}
+}
+table(2,20);
+
+
+// function table(number1,number2){
+//         for(let number2=0; number2<=20;number2++){
+//             let result = number2*number1;
+//             console.log(`${number1} x ${number2} = ${result}`);
+//         }
+    
+//     }
+//     table(2,20);
+
